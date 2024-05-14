5fab76746f543f2a81985650f850e25da80e5cf9
rafaeldgdf <rafaelvd2@hotmail.com>
2024-05-14 17:59:31 -0300

    finalizando o projeto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+                Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        
+        System.out.println("####INSIRA OS DADOS DA SUA CONTA BANCÁRIA#######");
+        System.out.print("Conta: ");
+        int conta = scanner.nextInt(); //precisa colocar o 'int' dentro do next
+
+        System.out.print("Agência: ");
+        String agencia = scanner.next();
+
+        System.out.print("Nome: ");
+        String nome = scanner.next(); 
+        scanner.nextLine(); // comando necessário para conseguir adicionar nome + sobrenome nesta única variável
+
+        System.out.print("Informe o saldo disponível: ");
+        double saldo = scanner.nextDouble();
+        
+
+        System.out.println("Olá " + nome + ", seu saldo de R$" + saldo + " já esta disponível para saque.");
+        System.out.println("Agência: " + agencia + "  /  Conta: " + conta);
+        
+
+        scanner.close();
     }
 }
